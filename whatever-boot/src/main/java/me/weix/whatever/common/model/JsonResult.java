c351024ce7ace04d6df9d751789bd65f84bd1970
weixiang <guibai.wx@raycloud.com>
2019-05-22 20:40:55 +0800

    userservice

---
+++ whatever-boot/src/main/java/me/weix/whatever/common/model/JsonResult.java
@@ -0,0 +1,36 @@
+package me.weix.whatever.common.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author : weixiang
+ * create at:  2019-05-22
+ * @description: 通用返回结果
+ */
+@Data
+public class JsonResult implements Serializable {
+
+    private Boolean success;
+
+    private String message;
+
+    private Object data;
+
+    public JsonResult(Boolean success, String message, Object data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public JsonResult(Boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public JsonResult(Boolean success) {
+        this.success = success;
+    }
+
+}
